4b13c7449ddd60e2f1b73867b30858536f02bd94
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-08-09 17:40:27 +0000

    Everything is done (for now)

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomExcuse = arr => {
+    return Math.floor(Math.random() * arr.length);
+  };
+
+  let first = who[randomExcuse(who)];
+  let second = action[randomExcuse(action)];
+  let third = what[randomExcuse(what)];
+  let forth = when[randomExcuse(when)];
+
+  document.querySelector("#excuse").innerHTML =
+    first + " " + second + " " + third + " " + forth;
+
+
 };
